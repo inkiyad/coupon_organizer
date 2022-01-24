97949ae33743cdadc0949560f6846ccd7c75bd55
Abdullah Rahman <abdullah2891@gmail.com>
2022-01-23 20:45:47 -0500

    camera-widget

--- layouts/ocr-module.js
+++ layouts/ocr-module.js
@@ -8,7 +8,36 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableHighlight
 } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#000'
+    },
+
+    capture: {
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        borderWidth: 5,
+        borderColor: "blue",
+        backgroundColor: "white",
+        marginBottom: 15
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    }
+
+});
 
 const OcrModule = () => {
   const [device , setDevice] = React.useState({id: "test"})
@@ -23,14 +52,22 @@ const OcrModule = () => {
       console.log(e)
 
   })
+  const onPress = image => console.log(image)
 
   //if (device == null) return <Text>loading<Text/>
   return (
     <Camera
-      style={StyleSheet.absoluteFill}
+      style={styles.preview}
       device={device}
       isActive={true}
-    />
+    >
+      <TouchableHighlight
+          style={styles.capture}
+          onPress={onPress}
+          >
+          <View />
+      </TouchableHighlight>
+    </Camera>
   )
   
 };

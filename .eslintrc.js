31281d836e9ff022f1876bdd288536eae832ea00
Abdullah Rahman <abdullah2891@gmail.com>
2022-01-23 22:25:20 -0500

    removed-merge-conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-=======
   root: true,
   extends: '@react-native-community',
->>>>>>> master
 };
